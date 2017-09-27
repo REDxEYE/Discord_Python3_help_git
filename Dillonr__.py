91c9137777f581eaca09486491fc009126a9986d
RED_EYE <med45c@gmail.com>
2017-09-27 23:42:41 +0300

    Init

--- Dillonr__.py
+++ Dillonr__.py
@@ -1,6 +1,6 @@
 import random
 
-
+import sys,os
 class entity:
     def __init__(self, hp, defence, atk, name):
         self.hp = hp
@@ -13,7 +13,7 @@ class entity:
         return self.hp > 0
 
     def damage(self, hp):
-        # print(f'{self.name} took {hp} damage!')
+        print(f'{self.name} took {hp} damage!')
         self.hp -= hp
 
     def rand_ev(self, chns):
@@ -28,6 +28,11 @@ class entity:
     def reset(self):
         self.hp = self.init_hp
 
+    def is_friend(self,entity):
+        return entity.__class__ is self.__class__
+
+    def heal(self,entity):
+        self.hp+=entity.hp//10
 
 class Knight(entity):
     def __init__(self, hp, defence, atk, dodge_chns, krit_chns, name):
@@ -37,19 +42,19 @@ class Knight(entity):
 
     def damage(self, hp):
         if self.rand_ev(self.dodge):
-            # print(f'{self.name} dodged {hp} damage!')
+            print(f'{self.name} dodged {hp} damage!')
             return
         else:
-            # print(f'{self.name} took {hp} damage!')
+            print(f'{self.name} took {hp} damage!')
             self.hp -= hp
 
     def attack(self, entity):
         if self.rand_ev(self.krit):
             atk = self.atk * 3 - entity.defence
-            # print(f'{self.name} attack: CRIT {self.atk*3} damage!')
+            print(f'{self.name} attack -> {entity.name}: CRIT {self.atk*3} damage!')
         else:
             atk = self.atk - entity.defence
-            # print(f'{self.name} attack: {self.atk} damage!')
+            print(f'{self.name} attack -> {entity.name}: {self.atk} damage!')
         entity.damage(atk)
 
 
@@ -62,10 +67,10 @@ class Dragon(entity):
     def attack(self, entity):
         if self.rand_ev(self.insta_kill):
             atk = entity.hp * 10
-            # print(f'{self.name} INSTA KILL ATTACK!: {atk} damage!')
+            print(f'{self.name} INSTA KILL ATTACK! -> {entity.name}: {atk} damage!')
             entity.damage(atk)
         else:
-            # print(f'{self.name} attack: {self.atk} damage!')
+            print(f'{self.name} attack -> {entity.name}: {self.atk} damage!')
             atk = self.atk - entity.defence
             entity.damage(atk)
 
@@ -86,27 +91,41 @@ class Arena:
             targets.remove(warior)
             target = random.choice(targets)  # type: entity
             if not target.is_alive():
-                # print(f'{target.name} died.')
+                print(f'{target.name} died.')
                 local_wariors.remove(target)
+            elif target.is_friend(warior):
+                # print(f"{warior.name} healed {target.name}: {warior.hp//10}")
+                # target.heal(warior)
+                continue
             else:
                 warior.attack(target)
-            # print('\n=========STATS==========')
-            for warior in self.wariors:
+            print('\n=========STATS==========')
+            for warior in local_wariors:
                 pass
-                # print(f'{warior.name}: {warior.hp} hp')
+                print(f'{warior.name}: {warior.hp} hp')
 
-            # print('========================\n')
+            print('========================\n')
+            if all([local_wariors[0].is_friend(warior) for warior in local_wariors]):
+                for warior in local_wariors:
+                    print(f"{warior.name} - won!")
+                    self.winrate[warior] += 1
+                return
 
-        # print(f"{local_wariors[0].name} - won!")
+        print(f"{local_wariors[0].name} - won!")
         self.winrate[local_wariors[0]] += 1
 
 
 if __name__ == '__main__':
     knight = Knight(300, 30, 80, 90, 33, "Dillinor__")
+    knight1 = Knight(300, 30, 80, 90, 33, "Powah")
     dragon = Dragon(1500, 60, 120, 25, 0, 'Dragon')
-    arena = Arena([knight, dragon])
-    for _ in range(1_000_000):
+    dragon1 = Dragon(1500, 60, 120, 25, 0, 'RED_EYE Dragon')
+    arena = Arena([knight,knight1, dragon,dragon1])
+    o = sys.stdout
+    sys.stdout = None
+    for _ in range(250):
         arena.fight()
+    sys.stdout = o
     print('\n============SCORE BOARD============')
     for warior, winrate in arena.winrate.items():
         print(f'{warior.name}: {winrate}\n')
